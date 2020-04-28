719f326b20861772f832bd6287a72fcb456c8fc4
Arnab Ghorai <arnabaghorai.com>
2020-04-29 04:42:56 +0530

    Updates

---
+++ precache-manifest.4c35a0088f5bd75ec3cb2d43c36a0287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc41eea665007cd9bdef82a74313d916",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/index.html"
+  },
+  {
+    "revision": "baf49c1512168e77f230",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "164ea3f4b4e90b492d1a",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/static/css/main.ece7d8a0.chunk.css"
+  },
+  {
+    "revision": "baf49c1512168e77f230",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/static/js/2.7b3b3149.chunk.js"
+  },
+  {
+    "revision": "5e2252a167034352fa7bed0a873eaa75",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/static/js/2.7b3b3149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164ea3f4b4e90b492d1a",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/static/js/main.647db1d4.chunk.js"
+  },
+  {
+    "revision": "b3de54f8f8d6aa67c3d2",
+    "url": "/arnabaghorai.github.io/Yolo-Tfjs-React/static/js/runtime-main.315d1870.js"
+  }
+]);
